ff5da9a29866801d4948cdb3599b5d25ea3593d1
Estelle <1211090065@qq.com>
2018-03-09 18:56:03 +0800

    Wip update homepage design

--- app/models/experinece.rb
+++ app/models/experinece.rb
@@ -6,13 +6,8 @@
 #  title        :string
 #  body         :text
 #  original_url :string
-#  person_id    :integer
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
-#
-# Indexes
-#
-#  index_experineces_on_person_id  (person_id)
 #
 
 class Experinece < ApplicationRecord
